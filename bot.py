c295321198c05d5b238f34bc2b9ca35c655c9084
palomatanis <paloma.pedregal@gmail.com>
2016-12-12 13:43:42 +0100

    BOT WORKING compra, reset, avisa comprados y gente que no ha respondido

--- bot.py
+++ bot.py
@@ -18,11 +18,25 @@ with open('./data/admins.json', 'r') as adminData:
 
     
 adictos_a_croissants = "./data/subscribers.json"
+# historico = "./data/historico.json"
 
-global num_crrr
+
+
+global lista_compra # Lista en texto para admin
+lista_compra = []
+
+
+global adictos_en_lista_compra # Lista para notificación
+adictos_en_lista_compra = []
+
+
 # User tracking
 global adictos_en_tramite
-adictos_en_tramite = {}
+adictos_en_tramite = {} 
+
+
+global adictos_no_contestados
+adictos_no_contestados = []
 
 
 
@@ -40,16 +54,26 @@ def notificarAdictos():
   [replyToQuestion(adict) for adict in subscribers_list]  
   return
 
+def notificarAdictosCompra():
+  [avisar_final(adict) for adict in adictos_no_contestados]  
+  [mensaje_compra(adict) for adict in adictos_en_lista_compra]  
+  return
+
 
 def replyToQuestion(adict):
+  adictos_no_contestados.append(adict)
   markup = types.ReplyKeyboardMarkup()
   markup.one_time_keyboard = True
   markup.row('¡SÍ!')
   markup.row('¡NO, PESADA!')
   adictos_en_tramite[adict] = 1 # El adicto ha progresado al nivel 1
-  print (adictos_en_tramite)
-  print (adictos_en_tramite.keys())
   bot.send_message(adict, "¿Croissant?", reply_markup=markup)
+
+def mensaje_compra(adict):
+  bot.send_message(adict, "Los Croissants van de camino ;)")
+
+def avisar_final(adict):
+  bot.send_message(adict, "Demasiado tarde, la compra ha acabado.")
   
 
 
@@ -111,8 +135,6 @@ def send_goodbye(message):
 @bot.message_handler(commands=['croissant'])
 def croissant_notif(message):
     if isAdmin_fromPrivate(message):
-      adictos_en_tramite = {}
-      num_crrr = 0
       bot.reply_to(message,
                    "Lanzando ofrecimiento de dulce felicidad en forma de Croissant Supremo...")
       
@@ -120,12 +142,37 @@ def croissant_notif(message):
     else:
       bot.reply_to(message, "No intentes suplantar al proveedor de Croissants")
 
+      
+@bot.message_handler(commands=['compra'])
+def compra_notif(message):
+  if isAdmin_fromPrivate(message):
+    notificarAdictosCompra()
+    bot.reply_to(message, "Avisando de compra.... \nRecomendado /reset")
+  else:
+    bot.reply_to(message, "No intentes suplantar al proveedor de Croissants")
+
+      
+@bot.message_handler(commands=['reset'])
+def compra_notif(message):
+  if isAdmin_fromPrivate(message):
+    
+    adictos_en_tramite = {}
+    lista_compra = []
+    adictos_en_lista_compra = []
+    adictos_no_contestados = []
+    bot.reply_to(message, "Reiniciando listas......")
+  else:
+    bot.reply_to(message, "No intentes suplantar al proveedor de Croissants")
+
+  
+
 ##### RESPUESTA NIVEL 1
 @bot.message_handler(func=lambda message: str(message.from_user.id) in adictos_en_tramite.keys() and adictos_en_tramite[str(message.from_user.id)] == 1)
 # @bot.message_handler(func=lambda message: adictos_en_tramite[message.from_user.id] == 1)
 def respuesta_niv1(message):
   respuesta = message.text
   adictL1 = str(message.from_user.id)
+  adictos_no_contestados.remove(adictL1)
   if (respuesta == '¡SÍ!'):
     markup2 = types.ReplyKeyboardMarkup()
     markup2.one_time_keyboard = True
@@ -136,7 +183,7 @@ def respuesta_niv1(message):
     bot.send_message(adictL1, "¿Cuántos?", reply_markup=markup2)
   else:
     adictos_en_tramite.pop(adictL1)
-    bot.send_message(adict, ":(")
+    bot.send_message(adictL1, ":(")
   
 
 @bot.message_handler(func=lambda message: str(message.from_user.id) in adictos_en_tramite.keys() and adictos_en_tramite[str(message.from_user.id)] == 2)
@@ -145,17 +192,23 @@ def respuesta_niv2(message):
   respuesta = message.text
   adictL2 = str(message.from_user.id)
   if (respuesta == '1'):
-    num_crrr = num_crrr + 1
-    bot.send_message(64219832, "Número de coissants ha ascendido a..." + num_crrr)
+    adictos_en_lista_compra.append(adictL2)
+    lista_compra.append(str(message.from_user.first_name) + " 1")
+    bot.send_message(6419832, "Lista de la compra\n:" + '\n'.join(lista_compra))
+    bot.send_message(adictL2, "¡Apuntado!")
   elif (respuesta == '2'):
-    num_crrr = num_crrr + 2
-    bot.send_message(64219832, "Número de coissants ha ascendido a..." + num_crrr)
-    bot.send_message(adict, ":(")
+    adictos_en_lista_compra.append(adictL2)
+    lista_compra.append(str(message.from_user.first_name) + " 2")
+    bot.send_message(6419832, "Lista de la compra:\n" + '\n'.join(lista_compra))
+    bot.send_message(adictL2, "¡Apuntado!")
   elif (respuesta == 'MUCHOS MÁS'):
+    adictos_en_lista_compra.append(adictL2)
+    lista_compra.append(str(message.from_user.first_name) + " 2")
+    bot.send_message(6419832, "Lista de la compra:\n" + '\n'.join(lista_compra))
     bot.send_message(adictL2, "Mejor ponte en contacto con @palome directamente")
   adictos_en_tramite.pop(adictL2)
 
-
+                
 # users_tracked.pop(uid)      
 
 
