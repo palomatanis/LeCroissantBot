c24253ba17bf675c2b6cd0df05bebc208ff1cf10
palomatanis <paloma.pedregal@gmail.com>
2017-02-21 20:15:47 +0100

    base sqlite

--- bot.py
+++ bot.py
@@ -3,23 +3,29 @@ from telebot import types
 import json
 import os
 import sys
+import sqlite3
+
 # import os.path as path
 
-
 ## Descripción: Este bot te introducirá en la lista de amantes de Croissants y te notificará cada tanto para tener la oportunidad de recibir uno en la FI por la mañana.
+
+# Base de datos
+adictos_a_croissants = sqlite3.connect('./data/subscribersDB', check_same_thread=False)
+
+
+# cursorAdictos = adictos_a_croissants.cursor()
+# cursorAdictos.execute('''
+#     CREATE TABLE users(id TEXT PRIMARY KEY, name TEXT, numCroissants INTEGER)''')
+# adictos_a_croissants.commit()
+
 
 # Abrir cosas
 with open("./bot.token", "r") as token:
   bot = telebot.TeleBot(token.readline().strip()) # Strip elimina mierda que se pueda colar en el token 
 
-
- with open('./data/admins.json', 'r') as adminData:
+with open('./data/admins.json', 'r') as adminData:
     admins = json.load(adminData)
-
     
-adictos_a_croissants = "./data/subscribers.json"
-historico = "./data/historico.json"
-
  # Lista en texto para admin
 global lista_compra
 lista_compra = []
@@ -30,13 +36,11 @@ adictos_en_lista_compra = []
 
 # User tracking
 global adictos_en_tramite
-adict os_en_tramite = {} 
+adictos_en_tramite = {} 
 
 # Suscriptores que aun no han contestado a la pregunta
 global adictos_no_contestados
 adictos_no_contestados = []
-
-
 
 def isAdmin_fromPrivate(message):
     if message.chat.type == 'private':
@@ -44,7 +48,6 @@ def isAdmin_fromPrivate(message):
         if str(userID) in admins:
             return True
         return False
-
 
 def notificarAdictos():
   with open(adictos_a_croissants, "r+") as subscribersRaw:
@@ -56,7 +59,6 @@ def notificarAdictosCompra():
   [avisar_final(adict) for adict in adictos_no_contestados]  
   [mensaje_compra(adict) for adict in adictos_en_lista_compra]  
   return
-
 
 def replyToQuestion(adict):
   adictos_no_contestados.append(adict)
@@ -75,9 +77,7 @@ def avisar_final(adict):
   bot.send_message(adict, "Demasiado tarde, la compra ha acabado.")
   
 
-
 ### cosas de comandos
-      
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     # import ipdb; ipdb.set_trace()
@@ -88,29 +88,23 @@ def send_welcome(message):
 @bot.message_handler(commands=['help'])
 def send_help(message):
   bot.send_message(message.chat.id, "Hola!\n Los comandos que puedes usar son:\n/help\n/subscribe\n/horario\n/unsubscribe")
+
+
   
 @bot.message_handler(commands=['subscribe'])
 def send_subscr(message):
-  if not os.path.isfile(adictos_a_croissants):
-    with open(adictos_a_croissants, "w") as subscribers:
-      subscribers.write(json.dumps([str(message.from_user.id)]))
-      subscribers.close()
-      # bot.send_message(6419832, "Se ha unido " +  + message.from_user.first_name + "!")
-      bot.send_message(message.chat.id, "Has sido añadido a la lista de suscriptores a noticias sobre croissants. ¡Enhorabuena!")
+  cursorAdictos = adictos_a_croissants.cursor()
+  oldSub = cursorAdictos.execute('''SELECT name FROM users WHERE id=?''', (message.from_user.id, ))
+
+  if oldSub:
+    bot.send_message(message.chat.id, "Pero si ya estás en la lista, ¡Caesa!")
   else:
-    with open(adictos_a_croissants, "r+") as subscribersRaw:
-      subscribers_list = json.load(subscribersRaw)
-      if str(message.from_user.id) in subscribers_list:
-        bot.send_message(message.chat.id, "Pero si ya estás en la lista, ¡Caesa!")
-        print (message.from_user.id)
-        subscribersRaw.close()
-      else:
-        subscribers_list.append(str(message.chat.id))
-        bot.send_message(message.chat.id, "Has sido añadido a la lista de suscriptores a noticias sobre croissants. ¡Enhorabuena!")
-        os.remove(adictos_a_croissants)
-        with open(adictos_a_croissants, "w") as subscribers:
-          subscribers.write(json.dumps(subscribers_list))
-        subscribers.close()      
+    cursorAdictos.execute('''INSERT INTO users(id, name, numCroissants)
+                  VALUES(?,?,?)''', (message.chat.id, message.chat.first_name, '0'))
+    adictos_a_croissants.commit()
+    bot.send_message(6419832, "Se ha unido " + message.from_user.first_name + " a la lista de adictos!")
+    bot.send_message(message.chat.id, "Has sido añadido a la lista de suscriptores a noticias sobre croissants. ¡Enhorabuena!")   
+    
        
 
 # Esto aun no sirve de nada pero me gusta el nombre
@@ -128,7 +122,13 @@ def horarios(message):
 # Ya se verá si se implementa esto algún día, por ahora supongo que nadie querrá salir de la lista nunca.    
 @bot.message_handler(commands=['unsubscribe'])
 def send_goodbye(message):
-    bot.send_message(message.chat.id, "Los croissants no te echarán de menos\n\n(de todas maneras esta función no está implementada..ups)")
+  cursorAdictos = adictos_a_croissants.cursor()
+  oldSub = cursorAdictos.execute('''SELECT name FROM users WHERE id=?''', (message.from_user.id, ))
+  if oldSub:
+    cursorAdictos.execute('''DELETE FROM users WHERE id = ? ''', (message.from_user.id))    
+    bot.send_message(message.chat.id, "Los croissants no te echarán de menos\n)")
+  else:
+    bot.send_message(message.chat.id, "Pero si aun no estás suscrito, ¡Caesa!\n)")
 
 
 # Cuando un admin va a ir a la Panadería, esto notifica a todos los suscriptores.
